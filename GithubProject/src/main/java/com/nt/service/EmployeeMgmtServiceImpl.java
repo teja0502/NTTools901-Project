d1a8a29972b84f82429082e54505dcaf710eb84f
teja <teja@gmail.com>
2023-10-06 14:46:15 +0530

    Service impl class commited

---
+++ GithubProject/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+import com.nt.model.Employee;
+
+public class EmployeeMgmtServiceImpl implements IEmployeeMgmtService {
+
+	@Override
+	public String registerEmployee(Employee employee) {
+		
+		return null;
+	}
+
+}
