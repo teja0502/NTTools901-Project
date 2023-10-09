66d4620969a6b01df2c540eac9f09dc8e3b00731
teja <teja@gmail.com>
2023-10-09 12:13:45 +0530

    commit

--- GithubProject/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
+++ GithubProject/src/main/java/com/nt/service/EmployeeMgmtServiceImpl.java
@@ -7,11 +7,8 @@ public class EmployeeMgmtServiceImpl implements IEmployeeMgmtService {
 	@Override
 	public String registerEmployee(Employee employee) {
 		System.out.println("EmployeeMgmtServiceImpl.registerEmployee()Modified");
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println(c);
 		return null;
+		
 	}
 
 }
